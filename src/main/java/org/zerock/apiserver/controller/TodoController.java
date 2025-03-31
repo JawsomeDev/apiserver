d9ee83183440ef51f678fc61d26c432c18e8b9c2
DeepSeeDev <hyuk2000s@naver.com>
2025-03-31 17:45:11 +0900

    RestController building

--- src/main/java/org/zerock/apiserver/controller/TodoController.java
+++ src/main/java/org/zerock/apiserver/controller/TodoController.java
@@ -3,14 +3,13 @@ package org.zerock.apiserver.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.apiserver.domain.dto.PageRequestDto;
 import org.zerock.apiserver.domain.dto.PageResponseDto;
 import org.zerock.apiserver.domain.dto.TodoDto;
 import org.zerock.apiserver.service.TodoService;
+
+import java.util.Map;
 
 @RestController
 @Log4j2
@@ -32,6 +31,14 @@ public class TodoController {
         log.info("list..........." + pageRequestDto);
 
         return todoService.getList(pageRequestDto);
+    }
+
+    @PostMapping("/")
+    public Map<String, Long> register(@RequestBody TodoDto dto){
+
+        Long tno = todoService.register(dto);
+
+        return Map.of("TNO", tno);
     }
 
 }
